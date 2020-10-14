f54f835ff11501f433ee490214632e98ad1ba775
Tarek Ali <tarekali15@outlook.com>
2020-10-14 17:59:25 -0500

    better code practicies (imported script)

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+function menuFunction(x) {
+  x.classList.toggle("change");
+}
+
+function scrollFunction(x) {
+  document.body.scrollTop = x;
+  document.documentElement.scrollTop = x;
+}
+
+function toggleDarkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function toggleMenuBar() {
+  if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+    let element = document.body;
+    element.classList.toggle("menu-bar");
+  }
+}
