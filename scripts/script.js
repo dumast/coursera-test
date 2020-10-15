94e81d1a023dfa16430d7067b1be970990f5bc16
Terence Dumas <72264649+dumast@users.noreply.github.com>
2020-10-15 08:00:47 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,18 @@ function toggleMenuBar() {
     element.classList.toggle("menu-bar");
   }
 }
+
+function SmoothHorizontalScrolling(e, time, amount, start) {
+    var eAmt = amount / 100;
+    var curTime = 0;
+    var scrollCounter = 0;
+    while (curTime <= time) {
+        window.setTimeout(SHS_B, curTime, e, scrollCounter, eAmt, start);
+        curTime += time / 100;
+        scrollCounter++;
+    }
+}
+
+function SHS_B(e, sc, eAmt, start) {
+    e.scrollLeft = (eAmt * sc) + start;
+}
