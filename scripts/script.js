2a5025b41c9ba380c72b237ced4fb8a07a5a2229
Terence Dumas <watsutx41@gmail.com>
2020-10-15 19:32:28 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,29 @@
 window.onload = function() {
-  if (localStorage.getItem("theme") == "dark") {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-  } 
+    if (localStorage.getItem("theme") == "dark") {
+        let element = document.body;
+        element.classList.toggle("dark-mode");
+    }
 }
 
 function menuFunction(x) {
-  x.classList.toggle("change");
+    x.classList.toggle("change");
 }
 
 function scrollFunction(x) {
-  document.body.scrollTop = x;
-  document.documentElement.scrollTop = x;
-  SmoothVerticalScrolling(myelement, 275, "center");
+    document.body.scrollTop = x;
+    document.documentElement.scrollTop = x;
+    SmoothVerticalScrolling(myelement, 275, "center");
 }
 
 function toggleDarkMode() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-  localStorage.setItem("theme", "dark");
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+    localStorage.setItem("theme", "dark");
 }
 
 function toggleMenuBar() {
-  if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
-    let element = document.body;
-    element.classList.toggle("menu-bar");
-  }
+    if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0) {
+        let element = document.body;
+        element.classList.toggle("menu-bar");
+    }
 }
