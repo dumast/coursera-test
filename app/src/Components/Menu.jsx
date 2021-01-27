d2d8c4f4a26f379d2d5826f9ec24139c51654018
Terence Dumas <watsutx41@@gmail.com>
2021-01-26 19:24:39 -0600

    update

--- app/src/Components/Menu.jsx
+++ app/src/Components/Menu.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 
 
 export default function Menu(props){
+
+    function Home(){
+        document.getElementById('home').scrollIntoView();
+    }
+
+    function About(){
+        document.getElementById('about').scrollIntoView();
+    }
+
+    function Programming(){
+        document.getElementById('programming').scrollIntoView();
+    }
+
+    function Contact(){
+        document.getElementById('contact').scrollIntoView();
+    }
+
     return (
+
         <div className = "menu">
             <ul>
-                <li><a href = "#home">Home</a></li>
-                <li><a href = "#about">About</a></li>
-                <li><a href = "#programming">Programming</a></li>
-                <li><a href = "#contact">Contact</a></li>
+                <li onClick = {Home}>Home</li>
+                <li onClick = {About}>About</li>
+                <li onClick = {Programming}>Programming</li>
+                <li onClick = {Contact}>Contact</li>
             </ul>
         </div>
     )
